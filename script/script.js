30e399483be9ce40ec1551374050d12b3e7b796f
Marcelo Dieguez <mdieguezdev@gmail.com>
2022-03-17 13:50:33 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,42 @@
+let score1 = document.querySelector('.score1')
+let score2 = document.querySelector('.score2')
+let onePointLeftBtn = document.querySelector('.one-point-left-btn')
+let twoPointLeftBtn = document.querySelector('.two-point-left-btn')
+let threePointLeftBtn = document.querySelector('.three-point-left-btn')
+
+let onePointRightBtn = document.querySelector('.one-point-right-btn')
+let twoPointRightBtn = document.querySelector('.two-point-right-btn')
+let threePointRightBtn = document.querySelector('.three-point-right-btn')
+
+let countLeft = 0;
+let countRight = 0
+
+onePointLeftBtn.addEventListener('click', function() {
+    countLeft += 1;
+    score1.textContent = countLeft;
+})
+
+twoPointLeftBtn.addEventListener('click', function() {
+    countLeft += 2;
+    score1.textContent = countLeft;
+})
+
+threePointLeftBtn.addEventListener('click', function() {
+    countLeft += 3;
+    score1.textContent = countLeft;
+})
+
+onePointRightBtn.addEventListener('click', function() {
+    countRight += 1;
+    score2.textContent = countRight;
+})
+
+twoPointRightBtn.addEventListener('click', function() {
+    countRight += 2;
+    score2.textContent = countRight;
+})
+
+threePointRightBtn.addEventListener('click', function() {
+    countRight += 3;
+    score2.textContent = countRight;
+})
