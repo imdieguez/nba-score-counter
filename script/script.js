97a5aa404330cc2186803ed83258fd4f6f57d408
Marcelo Dieguez <mdieguezdev@gmail.com>
2022-03-17 21:35:45 -0300

    Upload corrections
    Update HTML, CCS and JAVASCRIPT to show a "reset score" button.

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 let score1 = document.querySelector('.score1')
 let score2 = document.querySelector('.score2')
+
 let onePointLeftBtn = document.querySelector('.one-point-left-btn')
 let twoPointLeftBtn = document.querySelector('.two-point-left-btn')
 let threePointLeftBtn = document.querySelector('.three-point-left-btn')
@@ -8,8 +9,10 @@ let onePointRightBtn = document.querySelector('.one-point-right-btn')
 let twoPointRightBtn = document.querySelector('.two-point-right-btn')
 let threePointRightBtn = document.querySelector('.three-point-right-btn')
 
+let resetBtn = document.querySelector('#reset-btn')
+
 let countLeft = 0;
-let countRight = 0
+let countRight = 0;
 
 onePointLeftBtn.addEventListener('click', function() {
     countLeft += 1;
@@ -39,4 +42,11 @@ twoPointRightBtn.addEventListener('click', function() {
 threePointRightBtn.addEventListener('click', function() {
     countRight += 3;
     score2.textContent = countRight;
+})
+
+resetBtn.addEventListener('click', function() {
+    countLeft = 0;
+    countRight = 0;
+    score1.textContent = 0;
+    score2.textContent = 0;
 })
